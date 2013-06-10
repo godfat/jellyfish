15058d01dfb531705731f97dba64d45ccc122d2f
Lin Jen-Shin <godfat@godfat.org>
2013-06-11 02:45:37 +0800

    so that we can pass QUERY_STRING

--- lib/jellyfish/test.rb
+++ lib/jellyfish/test.rb
@@ -8,8 +8,13 @@ Bacon.summary_on_exit
 shared :jellyfish do
   %w[options get head post put delete patch].each do |method|
     instance_eval <<-RUBY
-      def #{method} path='/', app=app
-        app.call('PATH_INFO' => path, 'REQUEST_METHOD' => '#{method}'.upcase)
+      def #{method} path='/', app=app, query=''
+        File.open(File::NULL) do |input|
+          app.call('PATH_INFO'      => path              ,
+                   'REQUEST_METHOD' => '#{method}'.upcase,
+                   'QUERY_STRING'   => query             ,
+                   'rack.input'     => input             )
+        end
       end
     RUBY
   end
