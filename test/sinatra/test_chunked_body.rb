ed7c0081a94bee9a238d435f71d6d0caccd29192
Lin Jen-Shin <godfat@godfat.org>
2013-06-10 23:08:45 +0800

    add test_chunked_body.rb from sinatra's streaming_test.rb

---
+++ test/sinatra/test_chunked_body.rb
@@ -0,0 +1,44 @@
+
+require 'jellyfish/test'
+
+# stolen from sinatra
+describe 'Sinatra streaming_test.rb' do
+  behaves_like :jellyfish
+
+  should 'return the concatinated body' do
+    app = Class.new{
+      include Jellyfish
+      handle_exceptions false
+      get '/' do
+        Jellyfish::ChunkedBody.new{ |out|
+          out['Hello']
+          out[' ']
+          out['World!']
+        }
+      end
+    }.new
+    _, _, body = get('/', app)
+    body.to_a.join.should.eq 'Hello World!'
+  end
+
+  should 'postpone body generation' do
+    stream = Jellyfish::ChunkedBody.new{ |out|
+      10.times{ |i| out[i] }
+    }
+
+    stream.each.with_index do |s, i|
+      s.should.eq i
+    end
+  end
+
+  should 'give access to route specific params' do
+    app = Class.new{
+      include Jellyfish
+      get(%r{/(?<name>\w+)}){ |m|
+        Jellyfish::ChunkedBody.new{ |o| o[m[:name]] }
+      }
+    }.new
+    _, _, body = get('/foo', app)
+    body.to_a.join.should.eq 'foo'
+  end
+end
