b2e4089359029f4402ba4d697fe279e3d1968b5d
Lin Jen-Shin <godfat@godfat.org>
2013-06-12 04:34:30 +0800

    normalized_path would now make sure PATH_INFO begins with /

--- lib/jellyfish/normalized_path.rb
+++ lib/jellyfish/normalized_path.rb
@@ -6,7 +6,8 @@ require 'uri'
 module Jellyfish
   module NormalizedPath
     def path_info
-      URI.decode_www_form_component(super, Encoding.default_external)
+      path = URI.decode_www_form_component(super, Encoding.default_external)
+      if path.start_with?('/') then path else "/#{path}" end
     end
   end
 end
