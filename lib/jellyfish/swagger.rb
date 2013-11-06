f342c79c15907ba44601fcdae2ebb2e7ab1765a7
Lin Jen-Shin <godfat@godfat.org>
2013-11-07 04:19:49 +0800

    swagger.rb: better named

--- lib/jellyfish/swagger.rb
+++ lib/jellyfish/swagger.rb
@@ -5,25 +5,22 @@ module Jellyfish
     def inject jellyfish
       jellyfish_apis = jellyfish.routes.flat_map{ |meth, routes|
         routes.map do |(path, _)|
-          name, params = if path.respond_to?(:source)
-                           [path.source.gsub(/\(\?<(\w+)>(.+)\)/, '{\1}').
-                                        gsub(/\\\w+/, ''),
-                            path.names.map{ |name|
-                              {'name' => name,
-                               'type' => 'integer',
-                               'description' => 'argument description',
-                               'required' => true,
-                               'paramType' => 'path'}
-                            }]
-                         else
-                           [path.to_s, []]
-                         end
+          nickname, params = if path.respond_to?(:source)
+            [path.source.gsub(/\(\?<(\w+)>(.+)\)/, '{\1}').gsub(/\\\w+/, ''),
+             path.names.map{ |name|
+               {'name' => name, 'type' => 'integer',
+                'description' => 'argument description',
+                'required' => true, 'paramType' => 'path'}
+             }]
+          else
+            [path.to_s, []]
+          end
           {'method' => meth.to_s.upcase, 'summary' => 'api summary',
-           'name' => name[%r{^/[^/]+}], 'parameters' => params,
-           'nickname' => name}
+           'name' => nickname[%r{^/[^/]+}], 'parameters' => params,
+           'nickname' => nickname}
         end
-      }.group_by{ |api| api['name'] }.inject({}){ |r, (k, v)|
-        r[k] = v.group_by{ |vv| vv['nickname'] }
+      }.group_by{ |api| api['name'] }.inject({}){ |r, (name, operations)|
+        r[name] = operations.group_by{ |op| op['nickname'] }
         r
       }
 
@@ -41,8 +38,8 @@ module Jellyfish
       jellyfish.get %r{\A/swagger/(?<name>.+)\Z} do |match|
         name = "/#{match[:name]}"
         basePath = "#{request.scheme}://#{request.host_with_port}"
-        apis = jellyfish_apis[name].map{ |k, v|
-          {'path' => k, 'operations' => v}
+        apis = jellyfish_apis[name].map{ |nickname, operations|
+          {'path' => nickname, 'operations' => operations}
         }
 
         o = {'apiVersion' => '1.0.0', 'swaggerVersion' => '1.2',
