ef2e675ddf2b279949543426f7312bbaadf792cd
Lin Jen-Shin <godfat@godfat.org>
2014-03-11 23:20:26 +0800

    cleanup the code a bit

--- lib/jellyfish/swagger.rb
+++ lib/jellyfish/swagger.rb
@@ -4,49 +4,13 @@ require 'jellyfish/json'
 module Jellyfish
   class Swagger
     include Jellyfish
-    attr_reader :app, :swagger_apis, :jellyfish_apis
+    attr_reader :swagger_apis, :jellyfish_apis
     controller_include Jellyfish::NormalizedPath, Module.new{
       def block_call argument, block
         headers_merge 'Content-Type' => 'application/json; charset=utf-8'
         super
       end
     }
-
-    def initialize app
-      @app = app
-      @jellyfish_apis = app.routes.flat_map{ |meth, routes|
-        routes.map do |(path, _)|
-          nickname, params = if path.respond_to?(:source)
-            [path.source.gsub(/\(\?<(\w+)>(.+)\)/, '{\1}').gsub(/\\\w+/, ''),
-             path.names.map{ |name|
-               {'name' => name, 'type' => 'integer',
-                'description' => 'argument description',
-                'required' => true, 'paramType' => 'path'}
-             }]
-          else
-            [path.to_s, []]
-          end
-          {'method' => meth.to_s.upcase, 'summary' => 'api summary',
-           'name' => nickname[%r{^/[^/]+}], 'parameters' => params,
-           'nickname' => nickname}
-        end
-      }.group_by{ |api| api['name'] }.inject({}){ |r, (name, operations)|
-        r[name] = operations.group_by{ |op| op['nickname'] }
-        r
-      }
-
-      @swagger_apis = jellyfish_apis.keys.map do |name|
-        {'path' => name}
-      end
-    end
-
-    def swagger_info
-      if app.respond_to?(:info)
-        app.info
-      else
-        {}
-      end
-    end
 
     get '/' do
       [Jellyfish::Json.encode(
@@ -71,6 +35,43 @@ module Jellyfish
         'resourcePath'   => name                ,
         'produces'       => ['application/json'],
         'apis'           => apis                )]
+    end
+
+    def swagger_info
+      if app.respond_to?(:info)
+        app.info
+      else
+        {}
+      end
+    end
+
+    def swagger_apis
+      @swagger_apis ||= jellyfish_apis.keys.map do |name|
+        {'path' => name}
+      end
+    end
+
+    def jellyfish_apis
+      @jellyfish_apis ||= app.routes.flat_map{ |meth, routes|
+        routes.map do |(path, _)|
+          nickname, params = if path.respond_to?(:source)
+            [path.source.gsub(/\(\?<(\w+)>(.+)\)/, '{\1}').gsub(/\\\w+/, ''),
+             path.names.map{ |name|
+               {'name' => name, 'type' => 'integer',
+                'description' => 'argument description',
+                'required' => true, 'paramType' => 'path'}
+             }]
+          else
+            [path.to_s, []]
+          end
+          {'method' => meth.to_s.upcase, 'summary' => 'api summary',
+           'name' => nickname[%r{^/[^/]+}], 'parameters' => params,
+           'nickname' => nickname}
+        end
+      }.group_by{ |api| api['name'] }.inject({}){ |r, (name, operations)|
+        r[name] = operations.group_by{ |op| op['nickname'] }
+        r
+      }
     end
   end
 end
