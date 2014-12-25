9a143bdb968746028f28b18525170ca758a46df0
Lin Jen-Shin <godfat@godfat.org>
2014-12-26 01:41:03 +0800

    i am a bad programmer...

--- lib/jellyfish/test.rb
+++ lib/jellyfish/test.rb
@@ -9,15 +9,15 @@ Pork::Executor.__send__(:include, Muack::API)
 copy :jellyfish do
   module_eval(%w[options get head post put delete patch].map{ |method|
     <<-RUBY
-      def #{method} path='/', app=app, env={}
+      def #{method} path='/', a=app, env={}
         File.open(File::NULL) do |input|
-          app.call({'PATH_INFO'      => path              ,
-                    'REQUEST_METHOD' => '#{method}'.upcase,
-                    'SCRIPT_NAME'    => ''                ,
-                    'rack.input'     => input             ,
-                    'rack.url_scheme'=> 'https'           ,
-                    'SERVER_NAME'    => 'localhost'       ,
-                    'SERVER_PORT'    => '8080'}.merge(env))
+          a.call({'PATH_INFO'      => path              ,
+                  'REQUEST_METHOD' => '#{method}'.upcase,
+                  'SCRIPT_NAME'    => ''                ,
+                  'rack.input'     => input             ,
+                  'rack.url_scheme'=> 'https'           ,
+                  'SERVER_NAME'    => 'localhost'       ,
+                  'SERVER_PORT'    => '8080'}.merge(env))
         end
       end
     RUBY
