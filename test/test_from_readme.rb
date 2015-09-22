4f444ce9e6e64043c1ee3720d6d71d6e92d872e9
Lin Jen-Shin <godfat@godfat.org>
2015-09-22 23:23:05 +0800

    so that we could have multiple test cases

--- test/test_from_readme.rb
+++ test/test_from_readme.rb
@@ -20,34 +20,38 @@ describe 'from README.md' do
     next if title =~ /NewRelic/i
 
     would "pass from README.md #%02d #{title}" % index do
-      method_path, expect = test.strip.split("\n", 2)
-      method, path        = method_path.split(' ')
-      uri                 = URI.parse(path)
-      pinfo, query        = uri.path, uri.query
+      app = Rack::Builder.app{ eval(code) }
 
-      sock = nil
-      status, headers, body = File.open(File::NULL) do |input|
-        Rack::Builder.app{ eval(code) }.call(
-          'HTTP_VERSION'   => 'HTTP/1.1',
-          'REQUEST_METHOD' => method, 'PATH_INFO'  => pinfo,
-          'QUERY_STRING'   => query , 'SCRIPT_NAME'=> ''   ,
-          'rack.input'     => input ,
-          'rack.hijack'    => lambda{
-            sock = StringIO.new
-            # or TypeError: no implicit conversion of StringIO into IO
-            mock(IO).select([sock]){ [[sock], [], []] }
-            sock
-          })
+      test.split("\n\n").each do |t|
+        method_path, expect = t.strip.split("\n", 2)
+        method, path        = method_path.split(' ')
+        uri                 = URI.parse(path)
+        pinfo, query        = uri.path, uri.query
+
+        sock = nil
+        status, headers, body = File.open(File::NULL) do |input|
+          app.call(
+            'HTTP_VERSION'   => 'HTTP/1.1',
+            'REQUEST_METHOD' => method, 'PATH_INFO'  => pinfo,
+            'QUERY_STRING'   => query , 'SCRIPT_NAME'=> ''   ,
+            'rack.input'     => input ,
+            'rack.hijack'    => lambda{
+              sock = StringIO.new
+              # or TypeError: no implicit conversion of StringIO into IO
+              mock(IO).select([sock]){ [[sock], [], []] }
+              sock
+            })
+        end
+
+        if hijack = headers.delete('rack.hijack')
+          sock = StringIO.new
+          hijack.call(sock)
+          body = sock.string.each_line("\n\n")
+        end
+
+        body.extend(Enumerable)
+        [status, headers, body.to_a].should.eq eval(expect, binding, __FILE__)
       end
-
-      if hijack = headers.delete('rack.hijack')
-        sock = StringIO.new
-        hijack.call(sock)
-        body = sock.string.each_line("\n\n")
-      end
-
-      body.extend(Enumerable)
-      [status, headers, body.to_a].should.eq eval(expect, binding, __FILE__)
     end
   end
 end
